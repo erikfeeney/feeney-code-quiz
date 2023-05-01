77579656b56b05f9579bbfbbc77194a808f7a8a0
Erik Feeney <erikfeeney@jillians-air.mynetworksettings.com>
2023-05-01 19:57:20 -0400

    added the questions and answers for my quiz to the js file

---
+++ script.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    question: "What is my favorite color?",
+    answers: ["Green", "Orange", "Blue"],
+    correctAnswer: "Blue",
+  },
+  {
+    question: "What state was I born in?",
+    answers: ["Pennsylvania", "North Carolina", "New Jersey"],
+    correctAnswer: "New Jersey",
+  },
+  {
+    question: "True or False, I am 32 years old",
+    answers: ["True", "False"],
+    correctAnswer: "True",
+  },
+  {
+    question: "What is my favorite NFL team?",
+    answers: ["Eagles", "Giants", "Steelers"],
+    correctAnswer: "Giants",
+  },
+  {
+    question: "True or False, I am a good software developer",
+    answers: ["True", "False", "Not Yet!"],
+    correctAnswer: "Not Yet!",
+  },
+];
+
